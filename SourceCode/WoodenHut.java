9a44c7456552035fd9cc97f917154d20a6e6b982
nilamdeka23 <nilampratim.deka@sjsu.edu>
2016-10-09 08:17:31 -0700

    Added more house type

---
+++ SourceCode/WoodenHut.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class WoodenHut here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class WoodenHut extends House
+{
+    /**
+     * Act - do whatever the WoodenHut wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
