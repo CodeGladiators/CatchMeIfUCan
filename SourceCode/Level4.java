19364d90d6ba4501d9407ce0ea31d742ba254219
venkateshds <venkatesh.donthishashidhar@sjsu.edu>
2016-11-13 17:04:54 -0800

    level 3 changes

--- SourceCode/Level4.java
+++ SourceCode/Level4.java
@@ -9,6 +9,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Level4 extends World
 {
     private Counter actCounter;
+    private int timer = 3600;
     /**
      * Constructor for objects of class Level4.
      * 
@@ -21,6 +22,20 @@ public class Level4 extends World
          actCounter = new Counter("Hours Left: ");
         addObject(actCounter, 900, 20); 
     }
+    public void act(){
+        if(timer > 0){
+            timer--;
+        }
+        else{
+            Greenfoot.stop();
+        }
+        System.out.println("Your Score:" + timer);
+    }
+    public void bumpCounter()
+    {
+        actCounter.change();
+    }
+
     public void prepare()
     {
         
@@ -38,6 +53,20 @@ public class Level4 extends World
         houses[10]= new House();
         houses[11]= new House();
         houses[12]= new House();
+        
+        
+        Tree[] trees = new Tree[10];
+
+        trees[0]= new PineTree();
+        trees[1]= new PineTree();
+        trees[2]= new RimuTree();
+        trees[3]= new PineTree();
+        trees[4]= new PineTree();
+        trees[5]= new PineTree();
+        trees[6]= new PineTree();
+        trees[7]= new FigTree();
+        trees[8]= new FigTree();
+        trees[9]= new FigTree();
     
     
     }
