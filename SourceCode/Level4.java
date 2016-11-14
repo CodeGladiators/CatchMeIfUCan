43f6b4c5146bc2293ce4bf2aaca0a7de35508692
venkateshds <venkatesh.donthishashidhar@sjsu.edu>
2016-11-13 17:20:51 -0800

    level 3 changes

--- SourceCode/Level4.java
+++ SourceCode/Level4.java
@@ -54,6 +54,33 @@ public class Level4 extends World
         houses[11]= new House();
         houses[12]= new House();
         
+        // theif placement
+        int[] arr = {1,2,3,5,7,9,11,13};
+        int houseValue = arr[Greenfoot.getRandomNumber(arr.length)];
+
+        // Iterate over house objects and init properties
+        for(int i=0; i<13; i++) {
+            House house = houses[i];
+            house.setMoney((i+1)*1000);
+            house.setHouseNumber(i+1);
+
+            //add Money Counter
+            Counter houseCounter = new Counter(Float.toString(house.getMoney()));
+            houseCounter.updateHouseCounter();
+            //addObject(houseCounter, house.getX(), house.getY()-25);
+
+            if(house.getNumber()==houseValue){
+                house.setHasThief(true);
+            }else{
+                house.setHasThief(false);
+            }
+
+            if(house.getNumber()>houseValue){
+                house.setIsLooted(true);
+            }else{
+                house.setIsLooted(false);
+            }
+        }
         
         Tree[] trees = new Tree[10];
 
